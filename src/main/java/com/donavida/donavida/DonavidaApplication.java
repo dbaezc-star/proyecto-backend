dc1a6cd8f1842fc0816c5bc1b1045302befdd920
santi <santiagobaez2406@gmail.com>
2026-03-30 12:52:58 -0500

    feat: estructura inicial DonaVida

---
+++ src/main/java/com/donavida/donavida/DonavidaApplication.java
@@ -0,0 +1,13 @@
+package com.donavida.donavida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonavidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonavidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donavida/donavida/DonavidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donavida.donavida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonavidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
